c3c77e8763f887529d9622e9c97ecbc9b258e403
shintanur <90201287+shintanur@users.noreply.github.com>
2022-06-09 10:47:29 +0700

    Add files via upload

--- lib/data/resep_lunch.dart
+++ lib/data/resep_lunch.dart
@@ -14,103 +14,88 @@ List<resepLunch> dataLunch = [
   resepLunch(
       name: 'Salad Pasta',
       tutorial:
-          '1. Masukkan semua bahan ke dalam blender, haluskan hingga lembut dan mengental.'
-          ' 2. Tuang ke dalam gelas dan sajikan.',
+          '1. Rebus pasta terlebih dahulu dalam air mendidih yang sudah diberi sejumput garam. Masak sampai lunak atau al dente.'
+          '2. Tiriskan pasta yang sudah matang, lalu bilas dengan air dingin.'
+          '3. Campurkan semua bahan-bahan bumbu campuran, kemudian aduk bersama minyak zaitun secukupnya.'
+          '4. Dalam mangkuk salad, masukkan pasta, tomat ceri, paprika, dan zaitun. Tuang saus dari bumbu campur dan minyak zaitun, aduk sampai bumbu sudah merata.'
+          '5. Sajikan.',
       bahan: 
-          '1 Buah jeruk ukuran sedang,'
-          ' 1 buah tomat potong-potong,'
-          ' 1 buah apel potong menjadi 8 bagian,'
-          ' 4 buah wortel,' 
-          ' Air dan es secukupnya.'
+          ' 500 gram pasta gandum spiral atau bentuk lain sesuai selera,'
+          ' 8 buah tomat ceri, potong dadu,'
+          ' 1 buah paprika hijau, cincang,'
+          ' 1 paprika merah, potong dadu,' 
+          ' ½ paprika kuning, cincang,'
+          ' 10 butir zaitun hitam, cincang,'
+          ' minyak zaitun secukupnya.'
           ,
       image: 'assets/salad_pasta.jpg'),
   resepLunch(
       name: 'Sayur Asem',
       tutorial:
-          '1. Kocok telur di dalam mangkuk sedang, kemudian masukkan daun bawang, bayam, garam, dan lada. Aduk hingga merata.'
-          ' 2. Panaskan minyak dengan api sedang dan tuangkan adonan omelet.'
-          ' 3. Masak hingga permukaannya berwarna keemasan selama sekitar 2 menit.'
-          ' 4. Sajikan selagi hangat.'
+          ' 1. Masak air di panci hingga mendidih. Masukkan jagung dan melinjo, masak hingga matang.'
+          ' 2. Masukkan bumbu halus, lengkuas, daun salam, buah asam, dan asam jawa. Aduk rata.'
+          ' 3. Masukkan labu siam, masak hingga matang. Beri garam, gula pasir, dan gula jawa.'
+          ' 4. Masukkan terong, masak sebentar. Masukkan kacang panjang dan daun melinjo, masak selama 1 menit dan matikan api.'
           ,
       bahan: 
-          '60 gram bayam rebus,'
-          ' 4 butir telur,'
-          ' 50 gram keju,'
-          ' Lada dan garam secukupnya,' 
-          ' Daun bawang secukupnya.'
-          ' 2 sdm minyak sayur.'
+          ' 1 bonggol jagung, potong potong,'
+          ' 1 buah labu siam ukuran sedang potong-potong,'
+          ' 1/2 buah terong , potong-potong,'
+          ' 4 buah kacang panjang, potong-potong,' 
+          ' 10 buah melinjo,'
+          ' 1 genggam daun melinjo,'
+          ' 4 butir buah asam,'
+          ' 2 sdm asam jawa,'
+          ' 3 lembar daun salam,'
+          ' 2 ruas lengkuas , geprek,'
+          ' garam secukupnya,'
+          ' gula pasir secukupnya,'
+          ' gula jawa secukupnya,'
+          ' 1,5 liter air.'
           ,
       image: 'assets/sayurasem.jpg'),
   resepLunch(
       name: 'Gurame Cobek',
       tutorial:
-          '1. Rebus 2 gelas air dalam panci kecil, kemudian masukkan buah stroberi. Rebus selama 2 menit, kemudian tiriskan selama 5 menit.'
-          ' 2. Siapkan mangkuk besar dan masukkan tepung serbaguna, gula pasir, baking powder, dan garam. Aduk hingga rata.'
-          ' 3. Siapkan mangkuk kecil dan masukkan susu, telur, minyak, ekstrak vanili, dan buah stroberi. Aduk hingga rata.'
-          ' 4. Masukkan seluruh adonan dalam mangkuk kecil ke dalam mangkuk besar, lalu aduk hingga rata.'
-          ' 5. Siapkan wajan antilengket dan panaskan dengan api sedang.'
-          ' 6. Masukkan 4 sdm adonan untuk masing-masing pancake, masak hingga muncul gelembung-gelembung kecil di permukaannya selama 2-3 menit.'
-          ' 7. Bolak-balik pancake selama 1-2 menit hingga kematangannya merata.'
-          ' 8. Sajikan selagi hangat, bisa juga ditambahkan madu.'
+          '1. Bersihkan ikan, cuci bersih lalu tiriskan. Lumuri air jeruk nipis, biarkan 15 menit. Cuci lagi sampai bersih, tiriskan.'
+          ' 2. Lumuri ikan dengan garam dan lada bubuk secukupnya. Diamkan 10 menit, goreng sampai matang. Angkat dan tiriskan.'
+          ' 3. Goreng bawang merah setengah matang, angkat.'
+          ' 4. Haluskan jahe dan kencur, tambahkan cabai rawit dan bawang merah. Ulek kasar, bumbui garam, gula dan air jeruk limau. Tuangi air panas, aduk rata.'
+          ' 5. Tata ikan goreng di atas piring. Siram bumbu, sajikan segera.'
           ,
       bahan: 
-          '8 sdm buah stroberi, potong kasar,'
-          ' 4 sdm tepung serbaguna,'
-          ' 1 butir telur,'
-          ' 2 sdm gula pasir,' 
-          ' 2 sdm gula pasir,'
-          ' ¼ sdt ekstrak vanili,'
-          ' 6 sdm susu tanpa lemak,'
-          ' 1½ sdt minyak canola,'
-          ' Garam secukupnya,'
-          ' Mangkuk kecil,'
-          ' Wajan antilengket.'
+          '1 ekor ikan gurame 500 gram,'
+          ' 1 buah jeruk nipis,'
+          ' 8 butir bawang merah,'
+          ' 10-15 buah cabai rawit,' 
+          ' 2 cm kencur,'
+          ' 1 cm jahe,'
+          ' 2 buah jeruk limau,'
+          ' garam dan gula secukupnya,'
+          ' 1/2 gelas air panas matang.'
           ,
       image: 'assets/guramecobek.jpg'),
   resepLunch(
       name: 'Telur Ceplok Balado',
       tutorial:
-          '1. Rebus 2 gelas air dalam panci kecil, kemudian masukkan buah stroberi. Rebus selama 2 menit, kemudian tiriskan selama 5 menit.'
-          ' 2. Siapkan mangkuk besar dan masukkan tepung serbaguna, gula pasir, baking powder, dan garam. Aduk hingga rata.'
-          ' 3. Siapkan mangkuk kecil dan masukkan susu, telur, minyak, ekstrak vanili, dan buah stroberi. Aduk hingga rata.'
-          ' 4. Masukkan seluruh adonan dalam mangkuk kecil ke dalam mangkuk besar, lalu aduk hingga rata.'
-          ' 5. Siapkan wajan antilengket dan panaskan dengan api sedang.'
-          ' 6. Masukkan 4 sdm adonan untuk masing-masing pancake, masak hingga muncul gelembung-gelembung kecil di permukaannya selama 2-3 menit.'
-          ' 7. Bolak-balik pancake selama 1-2 menit hingga kematangannya merata.'
-          ' 8. Sajikan selagi hangat, bisa juga ditambahkan madu.',
-      bahan: '8 sdm buah stroberi, potong kasar,'
-          ' 4 sdm tepung serbaguna,'
-          ' 1 butir telur,'
-          ' 2 sdm gula pasir,'
-          ' 2 sdm gula pasir,'
-          ' ¼ sdt ekstrak vanili,'
-          ' 6 sdm susu tanpa lemak,'
-          ' 1½ sdt minyak canola,'
-          ' Garam secukupnya,'
-          ' Mangkuk kecil,'
-          ' Wajan antilengket.',
+          '1. Goreng telur ceplok, sisihkan.'
+          ' 2. Tumis bumbu halus, masukkan potongan tomat, beri garam dan gula, cek rasa. Aduk rata.'
+          ' 3. Tuang sedikit air, masukkan telur ceplok. Aduk sebentar, masak hingga bumbu menyerap, matikan api.',
+      bahan: '6 telur ayam,'
+          ' 1 tomat besar, potong kecil-kecil,'
+          ' garam secukupnya,'
+          ' gula secukupnya.',
       image: 'assets/telurceplok balado.jpg'),
   resepLunch(
       name: 'Udang Goreng Asam Gurih',
       tutorial:
-          '1. Rebus 2 gelas air dalam panci kecil, kemudian masukkan buah stroberi. Rebus selama 2 menit, kemudian tiriskan selama 5 menit.'
-          ' 2. Siapkan mangkuk besar dan masukkan tepung serbaguna, gula pasir, baking powder, dan garam. Aduk hingga rata.'
-          ' 3. Siapkan mangkuk kecil dan masukkan susu, telur, minyak, ekstrak vanili, dan buah stroberi. Aduk hingga rata.'
-          ' 4. Masukkan seluruh adonan dalam mangkuk kecil ke dalam mangkuk besar, lalu aduk hingga rata.'
-          ' 5. Siapkan wajan antilengket dan panaskan dengan api sedang.'
-          ' 6. Masukkan 4 sdm adonan untuk masing-masing pancake, masak hingga muncul gelembung-gelembung kecil di permukaannya selama 2-3 menit.'
-          ' 7. Bolak-balik pancake selama 1-2 menit hingga kematangannya merata.'
-          ' 8. Sajikan selagi hangat, bisa juga ditambahkan madu.',
-      bahan: '8 sdm buah stroberi, potong kasar,'
-          ' 4 sdm tepung serbaguna,'
-          ' 1 butir telur,'
-          ' 2 sdm gula pasir,'
-          ' 2 sdm gula pasir,'
-          ' ¼ sdt ekstrak vanili,'
-          ' 6 sdm susu tanpa lemak,'
-          ' 1½ sdt minyak canola,'
-          ' Garam secukupnya,'
-          ' Mangkuk kecil,'
-          ' Wajan antilengket.',
+          '1. Cuci bersih udang, belah punggung dan buang kotoran dan kulit, sisakan ekor.'
+          ' 2. Campur air asam jawa, bawang putih, sedikit garam, lada, udang. Aduk rata, marinasi kurang lebih 30 menit.'
+          ' 3. Panaskan dua sendok makan minyak goreng, tuang udang yang sudah dimarinasi. Masak sampai mendidih, tambahkan FiberCreme, bumbui saus tiram, kecap manis dan sedikit gulaPanaskan dua sendok makan minyak goreng, tuang udang yang sudah dimarinasi. Masak sampai mendidih, tambahkan FiberCreme, bumbui saus tiram, kecap manis dan sedikit gula.'
+          ' 4. Masak sampai saus mengental, masukkan cabai rawit. Angkat dan siap disajikan.',
+      bahan: '500 gram udang,'
+          ' 1 sendok makan FiberCreme,'
+          ' 1 sendok makan asam jawa(rendam dengan +-100 ml air, remas-remas dan buang ampasnya),'
+          ' 3 siung bawang putih (parut),',
       image: 'assets/udang.jpg'),
 ];
